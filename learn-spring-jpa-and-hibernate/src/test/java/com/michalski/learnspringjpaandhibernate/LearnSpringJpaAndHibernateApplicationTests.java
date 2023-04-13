7df22d38c9dcabd91b8f1d853ed1cd53d38cacaf
Delta.v <tmm.michalski@gmail.com>
2023-04-13 22:43:54 +0200

    add project for another section

---
+++ learn-spring-jpa-and-hibernate/src/test/java/com/michalski/learnspringjpaandhibernate/LearnSpringJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michalski.learnspringjpaandhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
